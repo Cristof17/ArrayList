ce3440697d5ea25e71f99fb4829ec3ffef7e32d3
Cristofor Rotsching <cristoforrotsching@MacBook-Pro-Cristofor.local>
2026-08-24 18:45:01 +0300

    removed commented code from ArrayList.h

--- include/ArrayList/ArrayList.h
+++ include/ArrayList/ArrayList.h
@@ -1,24 +1,9 @@
 #ifndef INCLUDE_ARRAYLIST_ARRAYLIST_H_
 #define INCLUDE_ARRAYLIST_ARRAYLIST_H_	1
 
-// #include "../defs.h"
-// #include "../types.h"
 #include <defs.h>
 #include <types.h>
 
-// struct ArrayList;
-//typedef struct ArrayList ArrayList;
-
-// DllExport int ArrayListGet(struct ArrayListPosition);
-// DllExport void ArrayListPut(struct ArrayListPosition, int);
-// DllExport Result ArrayListPerformCopyTo(struct ArrayList Dest, struct PAList source, struct PAElement);
-// struc  ArrayListPerformRuin(struct ArrayList List);
-// struct ArrayList ArrayListPerformDelete(struct ArrayList List);
-// struct PAData ArrayListGet(struct PAData[],PAInt);
-// struct PARecord ArrayListPerformArrange(PAInt);
-// struct PARecord ArrayListPutCount(struct ArrayList, struct PACount);
-// sjt
-// struct A
 DllExport struct ArrayList ArrayListPerformConstruct(struct ArrayList Array, int Data[], int Count);
 DllExport struct ArrayList ArrayListPerformInit(struct ArrayList, int Value, int Count);
 DllExport int ArrayListPerformGetFirst(struct ArrayList);
@@ -29,6 +14,4 @@ DllExport struct ArrayList ArrayListPerformCopy(struct ArrayList, struct ArrayLi
 DllExport ArrayListSize ArrayListPerformSize();
 DllExport HRESULT ArrayListPerformRuin(struct ArrayList Array);
 DllExport HRESULT ArrayListPerformDelete(struct ArrayList Array);
-// struct PAData
-// struct PAData ArrayList
 #endif
